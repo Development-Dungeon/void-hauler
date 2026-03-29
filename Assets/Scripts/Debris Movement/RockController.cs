563c23a4f93f22fddc1d2dbcddef15b00a3f798a
paulwrodriguez <8904872+paulwrodriguez@users.noreply.github.com>
2026-03-29 00:28:07 -0600

    Apply suggestion from @paulwrodriguez

--- Assets/Scripts/Debris Movement/RockController.cs
+++ Assets/Scripts/Debris Movement/RockController.cs
@@ -4,16 +4,6 @@ namespace Debris_Movement
 {
     public class RockController : MonoBehaviour
     {
-        // rotate
-        // i would love to have several different movements pattersn
-        // rotate
-        // move in a circle
-        // move in a line
-        // move randomly between points
-        // the collision also has to have some type of interaction
-        // the interactions can be either pick up
-        // the intereaction can be do some damage
-        // i would love to have these be different plug and play things
     
         public bool canRotate = true;
         public bool canPatrol = true;
