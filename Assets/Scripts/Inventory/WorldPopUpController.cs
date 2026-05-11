12c0b51c09fbf901ca42f588a250be6e345163ae
Paul Rodriguez <8904872+paulwrodriguez@users.noreply.github.com>
2026-05-11 13:25:15 -0600

    added custom colors for popus

--- Assets/Scripts/Inventory/WorldPopUpController.cs
+++ Assets/Scripts/Inventory/WorldPopUpController.cs
@@ -1,16 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using VContainer;
 using VContainer.Unity;
 
 namespace Inventory
 {
+    [Serializable]
+    public class TierToColor
+    {
+        public ItemType ItemType;
+        public Color Color;
+    }
     public class WorldPopUpController : MonoBehaviour
     {
         public static WorldPopUpController Instance;
-        public GameObject popUpPrefab;
+        public WorldPopUp popUpPrefab;
         public GameObject canvas;
         public float popUpDuration = 1f;
         private IObjectResolver _resolver;
+
+        [SerializeField] public List<TierToColor> TierToColors = new();
 
         [Inject]
         public void Construct(IObjectResolver objectResolver)
@@ -30,9 +41,16 @@ namespace Inventory
 
         public void AddEvent(Item entryItem, Vector3 transformPosition)
         {
+            var itemColor = TierToColors.FirstOrDefault(ttc =>
+            {
+                var pickedUpItem = entryItem.itemType.GetType();
+                var tierToFilter = ttc.ItemType.GetType();
+                return pickedUpItem == tierToFilter || tierToFilter.IsAssignableFrom(pickedUpItem);
+            })?.Color ?? Color.red;
+                
             var popup = _resolver.Instantiate(popUpPrefab, canvas.transform);
-            popup.GetComponent<WorldPopUp>().Setup(entryItem.itemType.name, Color.white, transformPosition);
-            Destroy(popup, popUpDuration);
+            popup.Setup(entryItem.itemType.name, itemColor, transformPosition);
+            Destroy(popup.gameObject, popUpDuration);
 
         }
         
